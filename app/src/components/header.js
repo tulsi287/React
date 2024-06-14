2755d5a5b1954d2adfd19f273f4ece68aadf3fc3
Tulsi <tulsiit@2872001@gmail.com>
2024-06-14 15:42:48 +0530

    Events

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+
+  inputchanges(){
+    console.log("changing in input");
+  }
   render() {
     return (
       <header>
         <div className="logo">Logo</div>
-        <input />
+        <input  onChange={this.inputchanges}/>
       </header>
     );
   }
