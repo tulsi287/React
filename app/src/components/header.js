fe7b66edb3e6f07bbba68178f808a8ce841eb2d9
Tulsi <tulsiit@2872001@gmail.com>
2024-06-14 10:58:00 +0530

    first commit

---
+++ app/src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">Logo</div>
+        <input />
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/header";
+import './styles/style.css';
+
+const App = () => (
+  <>
+    <Header />
+  </>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+ 
